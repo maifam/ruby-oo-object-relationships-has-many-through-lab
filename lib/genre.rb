97ba02649fe3bad499fb19a759a461507f8c115d
Mai Pham <maitphm@gmail.com>
2020-11-19 23:28:18 -0500

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,23 @@
+class Genre 
 
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
+
+
+    def self.all #class method lists each genre of class
+        @@all 
+    end 
+
+    def songs 
+        Song.all.select { |song| song.genre == self}
+    end 
+
+    def artists
+        Song.all.collect { |song| song.artist}
+    end 
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,17 @@
+class Song 
 
+    attr_accessor :name, :artist, :genre
+    @@all = [] #class variable begins w empty array
+
+    def initialize(name, artist, genre) #initalized
+        @name = name 
+        @artist = artist 
+        @genre = genre 
+        @@all << self
+    end 
+
+    def self.all #class method lists each song in class
+        @@all 
+    end 
+
+end 
