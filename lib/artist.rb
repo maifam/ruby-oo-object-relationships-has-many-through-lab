c8530cbaebc4e704c49fcc00714c73f4cf27ad64
Mai Pham <maitphm@gmail.com>
2020-11-19 23:25:59 -0500

    commit tree 3aaedf46c25b4dc815b8c7d2e41e6b27b53113bc

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,27 @@
+class Artist 
 
+    attr_accessor :name
+    @@all = [] #class variable of all empty array
+
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end 
+
+    def self.all #class method that lists each artist in class var
+        @@all 
+    end 
+
+    def new_song(name, genre) #instance method w arg name and genre
+        Song.new(name, self, genre) #creates new song that knows it belongs to self
+    end 
+
+    def songs #instance method
+        Song.all.select {|song| song.artist == self} #iterates through songs and finds artist belongs to it
+    end 
+
+    def genres
+        Song.all.collect {|song| song.genre}
+    end 
+
+end 
